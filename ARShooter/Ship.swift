463f3c53ef0ae595ef43efe00083ad1ad06bc458
xianlinbox <xianlinbox@gmail.com>
2017-08-15 16:32:27 +0800

    add ship into scene

--- ARShooter/Ship.swift
+++ ARShooter/Ship.swift
@@ -12,6 +12,10 @@ import SceneKit
 class Ship:SCNNode {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    override init() {
+        super.init()
         let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.1)
         self.geometry = box
         let shape = SCNPhysicsShape(geometry: box, options: nil)
@@ -20,7 +24,6 @@ class Ship:SCNNode {
         self.physicsBody?.categoryBitMask = CollisionCategory.ship.rawValue
         self.physicsBody?.contactTestBitMask = CollisionCategory.bullets.rawValue
     }
-    
 }
 
 struct CollisionCategory {
--- ARShooter/ViewController.swift
+++ ARShooter/ViewController.swift
@@ -12,7 +12,7 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate {
     
     @IBOutlet weak var sceneView:ARSCNView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.delegate = self;
@@ -29,11 +29,23 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
         
         sceneView.session.run(configuration);
     }
+    
+    func addNewShip() {
+        let newShip = Ship()
+        let positionX = floatBetween(-0.5, and: 0.5)
+        let positionY = floatBetween(-0.5, and: 0.5)
+        newShip.position = SCNVector3(positionX, positionY, -1)
+        sceneView.scene.rootNode.addChildNode(newShip)
+    }
+    
+    func floatBetween(_ x:Float, and y:Float) -> Float{
+        return (Float(arc4random())/Float(UInt32.max)) * (x - y) + y
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
 
