d537acad0668ec7949548ada8ca7ddb4e8c3c745
xianlinbox <xianlinbox@gmail.com>
2017-08-22 11:02:23 +0800

    update airplane position when camera move

--- ARDemo/CapatureCameraMove/CaptureCameraMoveViewController.swift
+++ ARDemo/CapatureCameraMove/CaptureCameraMoveViewController.swift
@@ -12,6 +12,7 @@ import ARKit
 class CaptureCameraMoveViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     @IBOutlet weak var sceneView:ARSCNView!
     @IBOutlet weak var goBackButton:UIButton!
+    weak var airplaneNode:SCNNode?
     
     override func viewDidLoad() {
         sceneView.delegate = self
@@ -26,22 +27,32 @@ class CaptureCameraMoveViewController: UIViewController, ARSCNViewDelegate, ARSe
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let airplaneScene = SCNScene(named: "");
-        let airplaneNode = airplaneScene!.rootNode.childNodes[0];
-        let airplaneScale = SCNVector3Make(0.5, 0.5, 0.5)
-        let airplanePosition = SCNVector3Make(0, -15, -15)
-        airplaneNode.position = airplanePosition
-        airplaneNode.scale = airplaneScale
-        
-        for node in airplaneNode.childNodes {
-            node.scale = airplaneScale
+        airplaneNode = airplaneScene?.rootNode.childNodes[0];
+        if let node = airplaneNode {
+            let airplaneScale = SCNVector3Make(0.5, 0.5, 0.5)
+            let airplanePosition = SCNVector3Make(0, -15, -15)
             node.position = airplanePosition
+            node.scale = airplaneScale
+            
+            for childNode in node.childNodes {
+                childNode.scale = airplaneScale
+                childNode.position = airplanePosition
+            }
+            sceneView.scene.rootNode.addChildNode(airplaneNode!)
         }
-        
-        sceneView.scene.rootNode.addChildNode(airplaneNode)
     }
     
     @IBAction func didTapGoBackButton() {
         print("Back to Home Screen!")
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    //Mark: AR session delegate
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        print("相机移动")
+        if let node  = airplaneNode {
+            let matrix = SCNMatrix4(frame.camera.transform)
+            node.position = SCNVector3Make(matrix.m31,matrix.m32,matrix.m33)
+        }
     }
 }
