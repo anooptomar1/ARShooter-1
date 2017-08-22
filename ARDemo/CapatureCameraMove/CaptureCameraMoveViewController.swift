1392ade91e66cbc807a9b0a9820064e10e011ab4
xianlinbox <xianlinbox@gmail.com>
2017-08-22 09:31:59 +0800

    add sceneview  and goback button to capture camera move demo

--- ARDemo/CapatureCameraMove/CaptureCameraMoveViewController.swift
+++ ARDemo/CapatureCameraMove/CaptureCameraMoveViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 import ARKit
 
 class CaptureCameraMoveViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
+    @IBOutlet weak var sceneView:ARSCNView!
+    @IBOutlet weak var goBackButton:UIButton!
+    
+    override func viewDidLoad() {
+        sceneView.delegate = self
+        sceneView.session.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let arSessionConfiguration = ARWorldTrackingSessionConfiguration()
+        arSessionConfiguration.planeDetection = ARWorldTrackingSessionConfiguration.PlaneDetection.horizontal
+        sceneView.session.run(arSessionConfiguration)
+    }
     
     
+    
+    @IBAction func didTapGoBackButton() {
+        print("Back to Home Screen!")
+        self.dismiss(animated: true, completion: nil)
+    }
 }
